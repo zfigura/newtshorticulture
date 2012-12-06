ebbf5bc19f89eda2b0859c2fda0ce52b7a4c1a01
ObsequiousNewt <obsequiousnewt@gmail.com>
2012-12-05 20:32:33 -0600

    Completely fixed fruit tree generator

--- src/common/horticulture/fruit/WorldGenFruitTrees.java
+++ src/common/horticulture/fruit/WorldGenFruitTrees.java
@@ -14,11 +14,10 @@ public class WorldGenFruitTrees{
 	}
 
 	public boolean generate(World w, Random r, int x, int y, int z, boolean isPomegranate){
-		if(!modnh.blockFruitSapling2.canGrowTree(w,x,y,z,7,true) && !isPomegranate){
-			System.out.println("bar");
-			return false;
-		}else{
-			System.out.println("Foo");
+//		if(!modnh.blockFruitSapling.canGrowTree(w,x,y,z,5,true) && !isPomegranate){
+//			System.out.println("bar");
+//			return false;
+//		}else{
 			int sap = modnh.blockFruitSapling.blockID;
 			int sap2 = modnh.blockFruitSapling2.blockID;
 			switch(w.getBiomeGenForCoords(x,z).biomeID){
@@ -27,54 +26,48 @@ public class WorldGenFruitTrees{
 				switch(r.nextInt(64)){
 				case 1:
 					w.setBlockAndMetadataWithNotify(x, y, z, sap, 15);
-					modnh.blockFruitSapling.attemptGrowTree(w, x, y, z, r, true);
 					break;
 				case 2:
 					w.setBlockAndMetadataWithNotify(x, y, z, sap, 7);
-					modnh.blockFruitSapling.attemptGrowTree(w, x, y, z, r, true);
 					break;
 				case 0:
 					//TODO clean up the spilled Coke
 					w.setBlockAndMetadataWithNotify(x, y, z, sap, 8);
-					modnh.blockFruitSapling.attemptGrowTree(w, x, y, z, r, true);
 				}
-				return true;
+				break;
 			case 4: //forest
 			case 18: //foresthills
 				//generate apple, pear, apricot, cherry, peach, plum, mulberry
 				w.setBlockAndMetadataWithNotify(x, y, z, sap, r.nextInt(7));
-				modnh.blockFruitSapling.attemptGrowTree(w, x, y, z, r, true);
-				return true;
+				break;
 			case 6: //swamp
 				//generate lemon, lime, orange, olive, kumquat
 				if(r.nextInt(5) == 2){
 					w.setBlockAndMetadataWithNotify(x, y, z, sap2, 1);
+					break;
 				}else{
 					w.setBlockAndMetadataWithNotify(x, y, z, sap, r.nextInt(4)+10);
 				}
-				modnh.blockFruitSapling.attemptGrowTree(w, x, y, z, r, false);
-				return true;
+				break;
 			case 21: //jungle
 			case 22: //jungle hills
 				switch(r.nextInt(3)){
 				case 0:
 					w.setBlockAndMetadataWithNotify(x, y, z, sap, 14);
-					modnh.blockFruitSapling.attemptGrowTree(w, x, y, z, r, true);
+					break;
 				case 1:
 					w.setBlockAndMetadataWithNotify(x, y, z, sap2, 0);
-					modnh.blockFruitSapling.attemptGrowTree(w, x, y, z, r, true);
+					break;
 				case 2:
 					w.setBlockAndMetadataWithNotify(x, y, z, sap2, 2);
-					modnh.blockFruitSapling.attemptGrowTree(w, x, y, z, r, true);
 				}
-				return true;
+				break;
 			case 8:
 				w.setBlockAndMetadataWithNotify(x, y, z, sap, 9);
-				modnh.blockFruitSapling.attemptGrowTree(w, x, y, z, r, true);
-				return true;
-			default:
-				return false;
 			}
-		}
+			if(modnh.blockFruitSapling.canGrowTree(w, x, y, z, 5, true)) modnh.blockFruitSapling.attemptGrowTree(w, x, y, z, r, true);
+			else w.setBlock(x,y,z,0);
+			return true;
+//		}
 	}
 }
